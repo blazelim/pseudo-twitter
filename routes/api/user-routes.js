3e01759d20fb7a5b2ebabd8c3a7e809ea7930dac
Blaze Lim <blazebentleycolim@gmail.com>
2021-06-16 22:31:11 -0700

    added user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    getAllUser,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller');
+
+// Set up Get all and post at /api/users
+router
+    .route('/')
+    .get(getAllUser)
+    .post(createUser);
+
+
+    // set up get one, put and delete at /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
